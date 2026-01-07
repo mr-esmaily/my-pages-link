709f6a36507ef849da4edac8b74c1bcc5b114c42
mr-esmaily <58333361+mr-esmaily@users.noreply.github.com>
2026-01-07 16:38:03 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,11 +1,12 @@
 const userID = '2a7401b5-6ebf-4244-88ce-2b99afe84499'; 
 const proxyIP = '155.254.35.75'; 
+const proxyPort = 8080; // پورتی که در پنل سنایی دادی
 
 export default {
   async fetch(request, env) {
     const upgradeHeader = request.headers.get('Upgrade');
     if (!upgradeHeader || upgradeHeader !== 'websocket') {
-      return new Response('Server is Running', { status: 200 });
+      return new Response('Bridge is Active', { status: 200 });
     }
     return await vlessOverWS(request);
   }
@@ -15,11 +16,13 @@ async function vlessOverWS(request) {
   const webSocketPair = new WebSocketPair();
   const [client, server] = Object.values(webSocketPair);
   server.accept();
-  const url = new URL(request.url);
-  const socket = await fetch(`https://${proxyIP}${url.pathname}`, {
+  
+  // اتصال مستقیم به سرور اصلی تو با پورت مخصوص
+  const socket = await fetch(`http://${proxyIP}:${proxyPort}`, {
     method: 'GET',
     headers: request.headers,
     body: request.body,
   });
+  
   return new Response(null, { status: 101, webSocket: client });
 }
